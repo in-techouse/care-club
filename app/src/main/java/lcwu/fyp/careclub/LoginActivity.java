ad852fd5e1038935a7b209f68097f8d0cd50726b
samav <samavianorr@gmail.com>
2019-11-24 11:59:03 +0500

    working on login

--- app/src/main/java/lcwu/fyp/careclub/LoginActivity.java
+++ app/src/main/java/lcwu/fyp/careclub/LoginActivity.java
@@ -1,19 +1,30 @@
 package lcwu.fyp.careclub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     Button signin ;
     TextView forgetpassword,signup;
     EditText email, password;
+    String strEmail,strPassword;
+    ProgressBar loginprogress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         password = findViewById(R.id.password);
         signup=findViewById(R.id.signup);
         forgetpassword = findViewById(R.id.forgetpassword);
+        loginprogress= findViewById(R.id.Loginprogress);
 
 
         signin.setOnClickListener(this);
@@ -36,18 +48,32 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         int id = v.getId();
         switch (id) {
             case R.id.signin: {
-                String strEmail = email.getText().toString();
-                String strPassword = password.getText().toString();
-                if (strEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
-                    email.setError("Enter a Valid email");
-                } else {
-                    email.setError(null);
-                }
-                if (strPassword.length() < 6) {
-                    password.setError("Enter valid Password");
-                } else {
-                    password.setError(null);
-                }
+                 strEmail = email.getText().toString();
+                 strPassword = password.getText().toString();
+                 Boolean flag=isValid();
+                 if (flag){
+                     loginprogress.setVisibility(View.VISIBLE);
+                     signin.setVisibility(View.GONE);
+                     //Firebase
+                     FirebaseAuth auth =FirebaseAuth.getInstance();
+                     auth.signInWithEmailAndPassword(strEmail,strPassword)
+                             .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                 @Override
+                                 public void onSuccess(AuthResult authResult) {
+                                     loginprogress.setVisibility(View.GONE);
+                                     signin.setVisibility(View.VISIBLE);
+                                     Log.e("login","success");
+                                 }
+                             }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    loginprogress.setVisibility(View.GONE);
+                                    signin.setVisibility(View.VISIBLE);
+                                    Log.e("login","failure " +e.getMessage());
+                                }
+                                });
+                 }
+
             }
          break;
             case R.id.signup:{
@@ -59,11 +85,28 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 Intent it=new Intent(LoginActivity.this,ForgetpasswordActivity.class);
                 startActivity(it);
                 break;
-        }
+            }
 
 
 
         }
+    }
+
+    private boolean isValid(){
+        boolean flag =true;
+        if (strEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
+            email.setError("Enter a Valid email");
+            flag=false;
+        } else {
+            email.setError(null);
+        }
+        if (strPassword.length() < 6) {
+            password.setError("Enter valid Password");
+            flag=false;
+        } else {
+            password.setError(null);
+        }
+        return flag;
     }
 }
 
