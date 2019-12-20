f7c35707c9f87a98742e2ab957833c727e6028ed
GEO <salihasaeed999@gmail.com>
2019-12-20 11:32:21 +0500

    Fragments Created

---
+++ app/src/main/java/lcwu/fyp/careclub/fragment/Ngos.java
@@ -0,0 +1,32 @@
+package lcwu.fyp.careclub.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import lcwu.fyp.careclub.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Ngos extends Fragment {
+
+
+    public Ngos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ngos, container, false);
+    }
+
+}
