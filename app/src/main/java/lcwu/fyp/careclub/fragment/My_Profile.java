1a94f598eba622b9b4e64be529f9813e9d5bd621
samav <samavianorr@gmail.com>
2019-12-28 12:02:37 +0500

    working on My profile

--- app/src/main/java/lcwu/fyp/careclub/fragment/My_Profile.java
+++ app/src/main/java/lcwu/fyp/careclub/fragment/My_Profile.java
@@ -10,11 +10,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import lcwu.fyp.careclub.R;
+import lcwu.fyp.careclub.director.Helpers;
+import lcwu.fyp.careclub.director.Session;
+import lcwu.fyp.careclub.model.User;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class My_Profile extends Fragment {
+    private Session session;
+    private User user;
+    private Helpers helpers;
+
 
 
     public My_Profile() {
@@ -26,7 +33,16 @@ public class My_Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my__profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_my__profile, container, false);
+        helpers=new Helpers();
+        session=new Session(getActivity());
+        user= session.getSession();
+
+
+
+
+        return root;
     }
+
 
 }
