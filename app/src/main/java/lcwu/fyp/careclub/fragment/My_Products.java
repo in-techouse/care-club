3587e56f4bc8d03565c504f3b255818577118ca4
GEO <salihasaeed999@gmail.com>
2020-01-26 13:05:14 +0500

    working on fragmenst

--- app/src/main/java/lcwu/fyp/careclub/fragment/My_Products.java
+++ app/src/main/java/lcwu/fyp/careclub/fragment/My_Products.java
@@ -3,6 +3,7 @@ package lcwu.fyp.careclub.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,7 +14,20 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import lcwu.fyp.careclub.R;
+import lcwu.fyp.careclub.director.Helpers;
+import lcwu.fyp.careclub.director.Session;
+import lcwu.fyp.careclub.model.Products;
+import lcwu.fyp.careclub.model.User;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +37,11 @@ public class My_Products extends Fragment {
 private  LinearLayout loading;
 private RecyclerView products;
 private TextView noRecordFound;
+private Session session;
+private Helpers helpers;
+private User user;
+private List<Products> product;
+private DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("Products");
     public My_Products() {
         // Required empty public constructor
     }
@@ -36,8 +55,34 @@ private TextView noRecordFound;
         loading=root.findViewById(R.id.loading);
         products=root.findViewById(R.id.products);
         noRecordFound=root.findViewById(R.id.noRecordFound);
+        session = new Session(getActivity());
+        helpers = new Helpers();
+        user=session.getSession();
+        product = new ArrayList<>();
+        loadProducts();
         return root;
-
     }
+    private void loadProducts(){
+        if (!helpers.isConnected(getActivity()))
+        {
+            helpers.showError(getActivity(),"Error","Error occured due to internet connection");
+            return;
+        }
+        loading.setVisibility(View.VISIBLE);
+        products.setVisibility(View.GONE);
+        noRecordFound.setVisibility(View.GONE);
+        reference.orderByChild("userid").equalTo(user.getId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loading.setVisibility(View.GONE);
+                products.setVisibility(View.VISIBLE);
+                noRecordFound.setVisibility(View.GONE);
+            }
+        });
+    }
 }
