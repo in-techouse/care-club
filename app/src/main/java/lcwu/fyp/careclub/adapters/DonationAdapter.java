e811c26e1c63e71df001be40335f889564b68ae0
samav <samavianorr@gmail.com>
2020-01-31 13:00:58 +0500

    working on adapters

--- app/src/main/java/lcwu/fyp/careclub/adapters/DonationAdapter.java
+++ app/src/main/java/lcwu/fyp/careclub/adapters/DonationAdapter.java
@@ -1,4 +1,51 @@
 package lcwu.fyp.careclub.adapters;
 
-public class DonationAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lcwu.fyp.careclub.R;
+import lcwu.fyp.careclub.model.Donations;
+
+public class DonationAdapter extends RecyclerView.Adapter<DonationAdapter.DonationHolder> {
+    private List<Donations> data;
+
+    public DonationAdapter() {
+        data= new ArrayList<>();
+    }
+
+    public void setData(List<Donations> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public DonationHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_donation,parent,false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DonationHolder holder, int position) {
+      final Donations d=data.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class DonationHolder extends RecyclerView.ViewHolder {
+
+        public DonationHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/lcwu/fyp/careclub/adapters/NgosAdapter.java
+++ app/src/main/java/lcwu/fyp/careclub/adapters/NgosAdapter.java
@@ -1,4 +1,52 @@
 package lcwu.fyp.careclub.adapters;
 
-public class NgosAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lcwu.fyp.careclub.R;
+import lcwu.fyp.careclub.fragment.Ngos;
+import lcwu.fyp.careclub.model.Donations;
+
+public class NgosAdapter extends RecyclerView.Adapter<NgosAdapter.NgosHolder> {
+    private List<Ngos> data;
+
+    public NgosAdapter() {
+        data= new ArrayList<>();
+
+    }
+
+    public void setData(List<Ngos> data) {
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public NgosHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_ngos,parent,false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NgosHolder holder, int position) {
+        final Ngos ngos=data.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class NgosHolder extends RecyclerView.ViewHolder{
+
+        public NgosHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
