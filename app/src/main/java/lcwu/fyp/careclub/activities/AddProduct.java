d0f8f947d39a2f9d15e7152272749de50596ed5f
GEO <salihasaeed999@gmail.com>
2020-02-07 13:06:59 +0500

    working on adapters

--- app/src/main/java/lcwu/fyp/careclub/activities/AddProduct.java
+++ app/src/main/java/lcwu/fyp/careclub/activities/AddProduct.java
@@ -18,34 +18,47 @@ import com.smarteist.autoimageslider.SliderViewAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lcwu.fyp.careclub.R;
 
-public class AddProduct extends AppCompatActivity implements BSImagePicker.ImageLoaderDelegate, BSImagePicker.OnMultiImageSelectedListener{
-
+public class AddProduct extends AppCompatActivity implements BSImagePicker.ImageLoaderDelegate, BSImagePicker.OnMultiImageSelectedListener,View.OnClickListener{
+    private List<Uri>productImages;
+    private SliderAdapter adapter;
+    AppCompatButton submitproduct;
+    EditText name,description,address,phoneno;
+    ProgressBar submitproductprogessbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+        productImages=new ArrayList<>();
+        submitproduct=findViewById(R.id.submitProduct);
+        name=findViewById(R.id.name);
+        description=findViewById(R.id.description);
+        address=findViewById(R.id.address);
+        phoneno=findViewById(R.id.phoneno);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         SliderView sliderView = findViewById(R.id.imageSlider);
-
-        SliderAdapter adapter = new SliderAdapter(AddProduct.this);
-
+        submitproduct.setOnClickListener(this);
+         adapter = new SliderAdapter(AddProduct.this);
         sliderView.setSliderAdapter(adapter);
+
 
         sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
         sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
@@ -81,8 +94,8 @@ public class AddProduct extends AppCompatActivity implements BSImagePicker.Image
       private void openGallery(){
           BSImagePicker multiSelectionPicker = new BSImagePicker.Builder("lcwu.fyp.careclub.fileprovider")
                   .isMultiSelect() //Set this if you want to use multi selection mode.
-                  .setMinimumMultiSelectCount(3) //Default: 1.
-                  .setMaximumMultiSelectCount(6) //Default: Integer.MAX_VALUE (i.e. User can select as many images as he/she wants)
+                  .setMinimumMultiSelectCount(1) //Default: 1.
+                  .setMaximumMultiSelectCount(2) //Default: Integer.MAX_VALUE (i.e. User can select as many images as he/she wants)
                   .setMultiSelectBarBgColor(android.R.color.white) //Default: #FFFFFF. You can also set it to a translucent color.
                   .setMultiSelectTextColor(R.color.primary_text) //Default: #212121(Dark grey). This is the message in the multi-select bottom bar.
                   .setMultiSelectDoneTextColor(R.color.colorAccent) //Default: #388e3c(Green). This is the color of the "Done" TextView.
@@ -109,6 +122,19 @@ public class AddProduct extends AppCompatActivity implements BSImagePicker.Image
 
     @Override
     public void onMultiImageSelected(List<Uri> uriList, String tag) {
+        productImages=uriList;
+        adapter.notifyDataSetChanged();
+
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
 
     }
 
@@ -128,36 +154,16 @@ public class AddProduct extends AppCompatActivity implements BSImagePicker.Image
         @Override
         public void onBindViewHolder(SliderAdapterVH viewHolder, int position) {
             Toast.makeText(AddProduct.this, "Position: " + position, Toast.LENGTH_LONG).show();
-            switch (position) {
-                case 0:
-                    Glide.with(viewHolder.itemView)
-                            .load("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
-                            .into(viewHolder.imageViewBackground);
-                    break;
-                case 1:
-                    Glide.with(viewHolder.itemView)
-                            .load("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
-                            .into(viewHolder.imageViewBackground);
-                    break;
-                case 2:
-                    Glide.with(viewHolder.itemView)
-                            .load("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
-                            .into(viewHolder.imageViewBackground);
-                    break;
-                default:
-                    Glide.with(viewHolder.itemView)
-                            .load("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
-                            .into(viewHolder.imageViewBackground);
-                    break;
-
-            }
+            Glide.with(viewHolder.itemView)
+                    .load(productImages.get(position))
+                    .into(viewHolder.imageViewBackground);
 
         }
 
         @Override
         public int getCount() {
             //slider view count could be dynamic size
-            return 4;
+            return productImages.size();
         }
 
         class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
